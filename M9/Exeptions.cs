0a13099eeb7f6caeb2207afa5b1f6f20a3378893
Nikolyase <96522347+Nikolyase@users.noreply.github.com>
2022-02-26 16:52:41 +0300

    Добавьте файлы проекта.

---
+++ M9/Exeptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace M9_task1
+{   /// <summary>
+    /// Собственный класс исключения
+    /// </summary>
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+
+}
---
+++ M9_task2/Exeptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace M9_task2
+{
+    /// <summary>
+    /// Мой класс исключения
+    /// </summary>
+    class MyClassException : Exception
+    {
+        public MyClassException()
+        { }
+
+        public MyClassException(string message)
+            : base(message)
+        { }
+
+    }
+}
